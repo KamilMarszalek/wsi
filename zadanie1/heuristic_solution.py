bbde623b62cbab4555915a01496a1531951f040f
Kamil Marszalek <kamilmar553@gmail.com>
2024-10-11 13:41:35 +0200

    Improved heuristic_solution using numpy

--- zadanie1/heuristic_solution.py
+++ zadanie1/heuristic_solution.py
@@ -1,20 +1,37 @@
 from heapq import heappush, heappop
 from node import Node
+import numpy as np
 
 
 def heuristic_solution(mass, mass_limit, price):
     max_price = 0
     temp_mass = 0
 
-    nodes = [Node(mass[i], price[i], price[i] / mass[i]) for i in range(len(mass))]
-    nodes.sort(reverse=True)
+    dtype = [("mass", int), ("price", int), ("price_to_mass_ratio", float)]
+    nodes = np.array(
+        [(mass[i], price[i], price[i] / mass[i]) for i in range(len(mass))], dtype=dtype
+    )
+    # nodes.sort(reverse=True)
+    nodes = np.sort(nodes, order="price_to_mass_ratio")[::-1]
     for node in nodes:
-        if temp_mass + node.mass <= mass_limit:
-            temp_mass += node.mass
-            max_price += node.price
+        if temp_mass + node["mass"] <= mass_limit:
+            temp_mass += node["mass"]
+            max_price += node["price"]
         if mass_limit - temp_mass < 1:
             break
     return max_price, temp_mass
+    # max_price = 0
+    # temp_mass = 0
+
+    # nodes = [Node(mass[i], price[i], price[i] / mass[i]) for i in range(len(mass))]
+    # nodes.sort(reverse=True)
+    # for node in nodes:
+    #     if temp_mass + node.mass <= mass_limit:
+    #         temp_mass += node.mass
+    #         max_price += node.price
+    #     if mass_limit - temp_mass < 1:
+    #         break
+    # return max_price, temp_mass
 
     # nodes = []
 
