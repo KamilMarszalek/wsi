b62e5d39b344080dbda44aed678a419edd57e84e
Kamil Marszalek <kamilmar553@gmail.com>
2024-10-11 12:49:46 +0200

    Working heuristic and generating

--- zadanie1/analyze_all_combs.py
+++ zadanie1/analyze_all_combs.py
@@ -1,14 +1,16 @@
 def analyze_all_combs(mass, mass_limit, price):
     bin_length = len(mass)
     max_price = 0
-    for i in range(2 ** bin_length):
+    max_mass = 0
+    for i in range(2**bin_length):
         temp_mass = 0
         temp_value = 0
-        bin_str = format(i, f'0{bin_length}b')
+        bin_str = format(i, f"0{bin_length}b")
         for count, char in enumerate(bin_str):
             temp_mass += int(char) * mass[count]
             temp_value += int(char) * price[count]
-        print(bin_str, ':', temp_value)
+        # print(bin_str, ':', temp_value)
         if temp_value > max_price and temp_mass <= mass_limit:
             max_price = temp_value
-    return max_price
+            max_mass = temp_mass
+    return max_price, max_mass
--- zadanie1/main.py
+++ zadanie1/main.py
@@ -4,7 +4,7 @@ from analyze_all_combs import analyze_all_combs
 from heuristic_solution import heuristic_solution
 from random import randint
 
-NUM_OF_ELEMENTS = 5
+NUM_OF_ELEMENTS = 20
 
 m = np.array([randint(1, 100) for _ in range(NUM_OF_ELEMENTS)])  # mass of the objects
 M = np.sum(m) / 2  # maximum mass of the objects
@@ -12,13 +12,20 @@ p = np.array([randint(1, 100) for _ in range(NUM_OF_ELEMENTS)])  # price of the 
 
 
 def main():
+    print("Mass limit:", M, "\n")
+    print("Generating all combinations:")
     start = time.process_time()
-    # result = analyze_all_combs(m, M, p)
+    result = analyze_all_combs(m, M, p)
+    end = time.process_time()
+    total = end - start
+    print("Max price:", result[0], "Max mass:", result[1])
+    print("Time:", "{0:02f}s".format(total))
+    print("\nHeuristic solution:")
+    start = time.process_time()
     result = heuristic_solution(m, M, p)
     end = time.process_time()
     total = end - start
-    print("Max price:", result[0])
-    print("Mass of prods:", result[1])
+    print("Max price:", result[0], "Mass of prods:", result[1])
     print("Time:", "{0:02f}s".format(total))
 
 
