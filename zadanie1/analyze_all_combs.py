ec734167dd817e7e2999a2537541d7caa82f2b62
Kamil Marszalek <kamilmar553@gmail.com>
2024-10-13 12:40:53 +0200

    done task 1

--- zadanie1/analyze_all_combs.py
+++ zadanie1/analyze_all_combs.py
@@ -1,16 +1,29 @@
+import numpy as np
+
+
+def increment_binary_mask(bin_mask, n):
+    for i in reversed(range(n)):
+        if bin_mask[i] == 0:
+            bin_mask[i] = 1
+            break
+        else:
+            bin_mask[i] = 0
+
+
 def analyze_all_combs(mass, mass_limit, price):
     bin_length = len(mass)
     max_price = 0
     max_mass = 0
-    for i in range(2**bin_length):
+    binary_mask = np.zeros(bin_length, dtype=int)
+    for _ in range(2**bin_length):
         temp_mass = 0
         temp_value = 0
-        bin_str = format(i, f"0{bin_length}b")
-        for count, char in enumerate(bin_str):
-            temp_mass += int(char) * mass[count]
-            temp_value += int(char) * price[count]
+        temp_mass = np.dot(binary_mask, mass)
+        temp_value = np.dot(binary_mask, price)
+
         # print(bin_str, ':', temp_value)
         if temp_value > max_price and temp_mass <= mass_limit:
             max_price = temp_value
             max_mass = temp_mass
+        increment_binary_mask(binary_mask, bin_length)
     return max_price, max_mass
--- zadanie1/heuristic_solution.py
+++ zadanie1/heuristic_solution.py
@@ -1,50 +1,21 @@
 from heapq import heappush, heappop
 from node import Node
-import numpy as np
 
 
 def heuristic_solution(mass, mass_limit, price):
     max_price = 0
     temp_mass = 0
-
-    dtype = [("mass", int), ("price", int), ("price_to_mass_ratio", float)]
-    nodes = np.array(
-        [(mass[i], price[i], price[i] / mass[i]) for i in range(len(mass))], dtype=dtype
-    )
-    # nodes.sort(reverse=True)
-    nodes = np.sort(nodes, order="price_to_mass_ratio")[::-1]
-    for node in nodes:
-        if temp_mass + node["mass"] <= mass_limit:
-            temp_mass += node["mass"]
-            max_price += node["price"]
+    nodes = []
+    for i in range(len(mass)):
+        heappush(
+            nodes, (-price[i] / mass[i], Node(mass[i], price[i], price[i] / mass[i]))
+        )
+    while nodes and temp_mass < mass_limit:
+        _, node = heappop(nodes)
+        if temp_mass + node.mass <= mass_limit:
+            temp_mass += node.mass
+            max_price += node.price
         if mass_limit - temp_mass < 1:
             break
+
     return max_price, temp_mass
-    # max_price = 0
-    # temp_mass = 0
-
-    # nodes = [Node(mass[i], price[i], price[i] / mass[i]) for i in range(len(mass))]
-    # nodes.sort(reverse=True)
-    # for node in nodes:
-    #     if temp_mass + node.mass <= mass_limit:
-    #         temp_mass += node.mass
-    #         max_price += node.price
-    #     if mass_limit - temp_mass < 1:
-    #         break
-    # return max_price, temp_mass
-
-    # nodes = []
-
-    # for i in range(len(mass)):
-    #     heappush(
-    #         nodes, (-price[i] / mass[i], Node(mass[i], price[i], price[i] / mass[i]))
-    #     )
-    # while nodes and temp_mass < mass_limit:
-    #     _, node = heappop(nodes)
-    #     if temp_mass + node.mass <= mass_limit:
-    #         temp_mass += node.mass
-    #         max_price += node.price
-    #     if mass_limit - temp_mass < 1:
-    #         break
-
-    # return max_price, temp_mass
