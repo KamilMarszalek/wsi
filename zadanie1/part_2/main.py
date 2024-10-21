483f7b7f183ec622e5403b26084f025f80d43079
Kamil Marszalek <kamilmar553@gmail.com>
2024-10-21 21:26:47 +0200

    Finished task 2

--- zadanie1/part_2/main.py
+++ zadanie1/part_2/main.py
@@ -1,4 +1,4 @@
-from steepest_ascent import steepest_ascent
+from steepest_ascent import steepest_ascent_classic, steepest_ascent_barzilai_borwein
 from plot import draw_contour, description, draw_arrows
 from cec2017.functions import f1, f2, f3
 from booth import booth
@@ -8,10 +8,10 @@ from constants import LIMIT, DIMENSIONS, MAX_ITER, COLORS
 from concurrent.futures import ThreadPoolExecutor
 
 
-# BETA = 5e-9  # f1
-# BETA = 5e-16  # f2
-BETA = 2e-8  # f3
-FUNCTION = f3
+BETA = 1e-9  # f1
+# BETA = 1e-22  # f2
+# BETA = 1e-8  # f3
+FUNCTION = f1
 DIM_1 = 0
 DIM_2 = 1
 
@@ -19,7 +19,7 @@ DIM_2 = 1
 def worker(i):
     x = np.random.uniform(-LIMIT, LIMIT, DIMENSIONS)
     print(f"Start {i}: ", x)
-    points = steepest_ascent(x, FUNCTION, BETA)
+    points = steepest_ascent_barzilai_borwein(x, FUNCTION, BETA)
     print(f"End {i}")
     print(f"Function value {i}:", FUNCTION(points[-1]))
     print(f"End {i}: ", points[-1])
@@ -38,22 +38,3 @@ if __name__ == "__main__":
     found_min = any(results)
     print("Found min:", found_min)
     plt.show()
-
-# if __name__ == "__main__":
-#     found_min = False
-#     draw_contour(FUNCTION, LIMIT, 1, DIM_1, DIM_2)
-#     for i in range(3):
-#         x = np.random.uniform(-LIMIT, LIMIT, DIMENSIONS)
-#         print("Start: ", x)
-#         points = steepest_ascent(x, FUNCTION, BETA)
-#         print("End")
-#         print("Function value:", FUNCTION(points[-1]))
-#         print("End: ", points[-1])
-#         draw_arrows(points, COLORS[i], DIM_1, DIM_2)
-#         description(
-#             x, points[-1], FUNCTION(points[-1]), BETA, i, COLORS[i], DIM_1, DIM_2
-#         )
-#         if len(points) < MAX_ITER:
-#             found_min = True
-#     print("Found min:", found_min)
-#     plt.show()
--- zadanie1/part_2/steepest_ascent.py
+++ zadanie1/part_2/steepest_ascent.py
@@ -3,23 +3,34 @@ import numpy as np
 from constants import LIMIT, MAX_ITER, PRECISION
 
 
-def steepest_ascent(
+def steepest_ascent_classic(
     point, function, beta, precision=PRECISION, minimize=True, limit=LIMIT
 ):
     points = [point]
     grad_fct = grad(function)
     step = np.subtract if minimize else np.add
-    counter = 0
     while not stop(function, grad_fct(points[-1]), points, precision):
         gradients = grad_fct(points[-1])
         points.append(np.clip(step(points[-1], beta * gradients), -limit, limit))
-        # print("Point:", points[-1])
-        # print("len of vect: ", np.linalg.norm(gradients))
-        counter += 1
-        if (counter % 5000 == 0 and counter < 10000) or (
-            counter % 1000 == 0 and counter >= 10000
-        ):
-            beta *= 2
+    return points
+
+
+def steepest_ascent_barzilai_borwein(
+    point, function, beta, precision=PRECISION, minimize=True, limit=LIMIT
+):
+    points = [point]
+    grad_fct = grad(function)
+    step = np.subtract if minimize else np.add
+    gradients_1 = grad_fct(points[-1])
+    points.append(np.clip(step(points[-1], beta * gradients_1), -limit, limit))
+    gradients_2 = grad_fct(points[-1])
+    while not stop(function, gradients_2, points, precision):
+        beta = np.dot(points[-1] - points[-2], gradients_2 - gradients_1) / np.dot(
+            gradients_2 - gradients_1, gradients_2 - gradients_1
+        )
+        gradients_1 = gradients_2
+        points.append(np.clip(step(points[-1], beta * gradients_1), -limit, limit))
+        gradients_2 = grad_fct(points[-1])
     return points
 
 
